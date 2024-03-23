f1c9a2d4a091b46cee45261f0a304a5cd56a5826
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2024-03-24 01:03:08 +0900

    GNB 버튼을 누르면 그에 맞는 페이지로 이동한다

---
+++ packages/app/src/hooks/useNavigate.ts
@@ -0,0 +1,22 @@
+import { useNavigate as useReactNavigate } from 'react-router-dom';
+import { ROUTES } from '@/router/router.constant';
+
+export const useNavigate = () => {
+  const navigate = useReactNavigate();
+
+  const moveHome = () => navigate(ROUTES.HOME.PATH);
+  const moveList = () => navigate(ROUTES.LIST.PATH);
+  const moveCart = () => navigate(ROUTES.CART.PATH);
+  const moveOrder = () => navigate(ROUTES.ORDER.PATH);
+  const moveOrderList = () => navigate(ROUTES.ORDER_LIST.PATH);
+  const moveOrderDetail = () => navigate(ROUTES.ORDER_DETAIL.PATH);
+
+  return {
+    moveHome,
+    moveList,
+    moveCart,
+    moveOrder,
+    moveOrderList,
+    moveOrderDetail,
+  };
+};
--- packages/app/src/layouts/Layout.tsx
+++ packages/app/src/layouts/Layout.tsx
@@ -1,18 +1,29 @@
 import { Navigation } from '@/components/navigation/Navigation';
+import { useNavigate } from '@/hooks/useNavigate';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export const Layout = ({ children }: LayoutProps) => {
+  const { moveHome, moveCart, moveOrderList } = useNavigate();
+
   return (
     <>
       <Navigation
-        left={<h1 className='nav-title'>CLEAN CODE SHOP</h1>}
+        left={
+          <h1 className="nav-title" onClick={moveHome}>
+            CLEAN CODE SHOP
+          </h1>
+        }
         right={
-          <div className='flex gap-15'>
-            <button className='nav-button'>장바구니</button>
-            <button className='nav-button'>주문목록</button>
+          <div className="flex gap-15">
+            <button className="nav-button" onClick={moveCart}>
+              장바구니
+            </button>
+            <button className="nav-button" onClick={moveOrderList}>
+              주문목록
+            </button>
           </div>
         }
       />
