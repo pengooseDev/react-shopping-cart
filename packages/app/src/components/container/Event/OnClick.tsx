00196667397b58655d2c6d6d0d1099ff46247189
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2024-04-01 05:28:18 +0900

    refactor: Event.OnClick 태그 개선(div => button)

--- packages/app/src/components/container/Event/OnClick.tsx
+++ packages/app/src/components/container/Event/OnClick.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import { OnClickProps } from './event.type';
 
 export const OnClick = ({ onClick, children }: OnClickProps) => {
-  return React.createElement('div', { onClick, className: 'hover' }, children);
+  return React.createElement(
+    'button',
+    { onClick, className: 'hover' },
+    children
+  );
 };
