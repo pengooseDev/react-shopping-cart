eade9a35cf2d582a3517b111464d1f78a04bd154
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2024-04-02 03:09:17 +0900

    fix: navigate type 수정

--- packages/app/src/layouts/TopNavigation.tsx
+++ packages/app/src/layouts/TopNavigation.tsx
@@ -7,16 +7,16 @@ export const TopNavigation = () => {
   return (
     <Navigation
       left={
-        <h1 className="nav-title hover" onClick={moveHome}>
+        <h1 className="nav-title hover" onClick={() => moveHome()}>
           CLEAN CODE SHOP
         </h1>
       }
       right={
         <div className="flex gap-15">
-          <button className="nav-button hover" onClick={moveCart}>
+          <button className="nav-button hover" onClick={() => moveCart()}>
             장바구니
           </button>
-          <button className="nav-button hover" onClick={moveOrderList}>
+          <button className="nav-button hover" onClick={() => moveOrderList()}>
             주문목록
           </button>
         </div>
